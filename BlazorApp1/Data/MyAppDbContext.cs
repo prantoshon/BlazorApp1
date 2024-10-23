3b609f72a3f64c2705acef4b326f531dc4fba5b8
DELL <DELL@MHPC>
2024-10-23 23:08:17 +0600

    Add project files.

---
+++ BlazorApp1/Data/MyAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorApp1.Models;
+
+namespace BlazorApp1.Data
+{
+    public class MyAppDbContext : DbContext
+    {
+        public MyAppDbContext (DbContextOptions<MyAppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorApp1.Models.Product> Product { get; set; } = default!;
+    }
+}
